62242a57da54a9cd610f1bb7dea0c63c71d616ea
electricSamarth <samarthmathur13@gmail.com>
2019-10-12 08:50:16 +0530

    added some bootstrap and database

--- registration.php
+++ registration.php
@@ -1,46 +1,72 @@
+<?php
+require_once('config.php');
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>User Registration</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
 <body>
 	<div>
 	<?php
 	if (isset($_POST['create'])){
-		echo "user submitted";
+		$firstname	=$_POST['firstname'];
+		$lastname	=$_POST['lastname'];
+		$email 		=$_POST['email'];
+		$gamerid	=$_POST['gamerid'];
+		$password 	=$_POST['password'];
 
+		$sql="INSERT INTO users (firstname,lastname,email,gamerid,password) VALUES (?,?,?,?,?)";
+		$stmtinsert = $db->prepare($sql);
+		$result=$stmtinsert->execute([$firstname,$lastname,$email,$gamerid,$password]);
+		if($result){
+			echo "Successfully saved.";
+		}else{
+			echo "There was error while saving the data";
+		}
+				
 	}
 	?>
 	</div>
 
 <div>
-	<form action="registratio.hp" method="post">
+	<form action="registration.php" method="post">
 		<div class="container">
-			<h1> Registration</h1>
-			<p>Fill Up the form </p>
+			<div class="row">
+				<div class="col-sm-3">
+					<h1> Registration</h1>
+					<p>Fill Up the form </p>
+					
+					<hr class="mb-3">
 
-			<label for ="firstname"><b> First Name</b> </label> 
-			<input type="text" name="firstname" required>
-			
+					<label for ="firstname"><b> First Name</b> </label> 
+					<input class ="form-control"  type="text" name="firstname" required>
+					
 
-			<label for ="lastname"><b> Last Name</b> </label> 
-			<input type="text" name="lastname" required>
-			
+					<label for ="lastname"><b> Last Name</b> </label> 
+					<input class ="form-control"  type="text" name="lastname" required>
+					
 
-			<label for ="email"><b> Email</b> </label> 
-			<input type="email" name="email" required>
-			
+					<label for ="email"><b> Email</b> </label> 
+					<input class ="form-control"  type="email" name="email" required>
+					
 
-			<label for ="GamerId"><b> GamerId</b> </label> 
-			<input type="text" name="gamerid" required>
+					<label for ="gamerid"><b> GamerId</b> </label> 
+					<input type="text" class ="form-control"  name="gamerid" required>
 
 
-			<label for ="password"><b>Password</b> </label> 
-			<input type="password" name="password" required>
-			
-			<input type="submit" name="create" value="Sign Up">
-			
+					<label for ="password"><b>Password</b> </label> 
+					<input type="password" class ="form-control"  name="password" required>
+					
+					<hr class="mb-3">
+					
+					<input type="submit" name="create" value="Sign Up" class="btn btn-primary">
+
+				</div>
+			</div>
 		</div>
+
 		
 	</form>
 
